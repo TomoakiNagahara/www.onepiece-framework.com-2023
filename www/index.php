09f1ea9c38e3d443068a76cfc7ce3b22db396c94
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-12-29 16:16:21 +0900

    New: www > .htaccess, app.php, index.php, index.phtml, index.html

---
+++ www/index.php
@@ -0,0 +1,22 @@
+<?php
+/** op-skeleton-2023:/www/index.php
+ *
+ * @created		2023-12-29
+ * @version		1.0
+ * @package		op-app-skeleton-2023
+ * @author		Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright	Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+OP()->Template('index.phtml');
