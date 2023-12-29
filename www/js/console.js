0a0fc3d3c355d187d2a78070250635c74a9b21ad
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-12-29 16:16:47 +0900

    New: www > js > index.php, console.js

---
+++ www/js/console.js
@@ -0,0 +1,11 @@
+
+/** op-skeleton-2023:/www/js/console.js
+ *
+ * @created   2023-12-29
+ * @version   1.0
+ * @package   op-skeleton-2023
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+console.log('JS loading is successful.');
