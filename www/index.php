204d82b7b29f1506a918b7d610eab063bd2f36e6
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-01-04 03:20:57 +0900

    Fix: www > index.php | Whether there is an arguments.

--- www/index.php
+++ www/index.php
@@ -18,5 +18,38 @@ declare(strict_types=1);
  */
 namespace OP;
 
-//	...
-OP()->Template('index.phtml');
+/************************************************/
+//	.htaccess file has not been initialized.	//
+if( defined('_OP_NAME_SPACE_') === false ){
+	include('app.php');
+	exit(__FILE__.' #'.__LINE__);
+}
+//	You should leave this logic. It's for you.	//
+/***********************************************/
+
+//	Get SmartURL arguments.
+$args = OP::Router()->Args();
+
+//	Whether there is an arguments.
+if( empty($args) ){
+
+	/** If there are no arguments.
+	 *
+	 */
+	OP::Template('index.phtml');
+
+}else if( $args[0] === 'cd.php' ){
+
+	/** cd.php file is in current directory.
+	 *
+	 */
+	OP::Template('cd.php');
+
+}else{
+
+	/** 404.php file is in current directory.
+	 *
+	 */
+	OP::Template('404.php');
+
+}
