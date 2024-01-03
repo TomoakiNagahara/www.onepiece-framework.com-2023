7f4f61c23f15f211c2e68986c2511d0b0a37196a
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-01-03 09:31:57 +0900

    New: www > 403.php, 404.php

---
+++ www/403.php
@@ -0,0 +1,15 @@
+<?php
+/** op-skeleton-2020:/403.php
+ *
+ * @creation  2018-11-12
+ * @version   1.0
+ * @package   op-skeleton-2020
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+//	...
+$path = '403_forbidden.phtml';
+
+//	...
+OP()->Template($path);
---
+++ www/404.php
@@ -0,0 +1,42 @@
+<?php
+/** op-skeleton-2020:/404.php
+ *
+ * @created   2018-10-30
+ * @version   1.0
+ * @package   op-skeleton-2020
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ * @created   2019-02-18
+ */
+namespace OP;
+
+//	Change http status code.
+http_response_code(404);
+
+//	...
+if( date('m', OP()->Time()) == '10' ){
+	$path = '404_halloween.phtml';
+}else{
+	$path = '404_notfound.phtml';
+}
+
+//	Display 404 page.
+OP()->Template($path);
+
+//	404 Error notice.
+if( OP()->Config('execute')['notfound'] ?? null ){
+	Unit('NotFound')->Auto();
+}else{
+	//	...
+	$scheme = $_SERVER['REQUEST_SCHEME'] ?? '(empty)';
+	$host   = $_SERVER['HTTP_HOST']      ?? '(empty)';
+	$url    = $_SERVER['REQUEST_URI']    ?? '(empty)';
+	$url    = $scheme .'://'. $host . $url;
+
+	//	...
+	throw new \Exception("404 Not Found Error - $url");
+}
