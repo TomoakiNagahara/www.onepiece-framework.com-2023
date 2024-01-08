29baede2be789add7de70ad7c6fca01b560063a6
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-01-08 11:23:33 +0900

    New: asset > config > execute.php

---
+++ asset/config/execute.php
@@ -0,0 +1,17 @@
+<?php
+/** op-skeleton-2023:/asset/config/execute.php
+ *
+ * @created		2023-01-26	www.onepiece-framework.com-2022
+ * @copied		2023-12-30
+ * @version		1.0
+ * @package		op-skeleton-2023
+ * @author		Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright	Tomoaki Nagahara All right reserved.
+ */
+
+/** Return config array.
+ *
+ * @return    array        $config
+ */
+return [
+];
