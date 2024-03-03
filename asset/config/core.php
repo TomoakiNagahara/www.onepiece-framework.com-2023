dcf3e49c1fab378df31de3632c01a7d7899ed7e3
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-03-03 16:23:11 +0900

    New: asset > config > core.php

---
+++ asset/config/core.php
@@ -0,0 +1,24 @@
+<?php
+/** op-skeleton-2023:/asset/config/core.php
+ *
+ * @created		2024-03-03
+ * @version		1.0
+ * @package		op-skeleton-2023
+ * @author		Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright	Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+$meta_path = [
+	'CanConvertFromFullPath' => false,
+];
+
+//	...
+return [
+	'MetaPath' => $meta_path,
+];
