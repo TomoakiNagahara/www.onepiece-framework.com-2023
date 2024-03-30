a855a44a2d1e0264f96f3453508a8f3961bc14e1
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-03-31 03:02:55 +0900

    New: asset > git > init.php

---
+++ asset/git/init.php
@@ -0,0 +1,23 @@
+<?php
+/** op-skeleton-2023:/asset/git/init.php
+ *
+ * <pre>
+ * php asset/git/init.php
+ * </pre>
+ *
+ * @created    2023-12-25
+ * @version    1.0
+ * @package    op-skeleton-2023
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
