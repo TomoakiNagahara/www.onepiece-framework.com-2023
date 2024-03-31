e60967dbaf14ea2ab8df68646ba39a22f7dccc96
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-04-01 00:48:55 +0900

    New: asset > git > hook > set_hook-hooks.php

---
+++ asset/git/hook/set_hook-hooks.php
@@ -0,0 +1,46 @@
+<?php
+/** op-skeleton-2023:/asset/git/hook/set_hook-hooks.php
+ *
+ * @created    2024-04-01
+ * @version    1.0
+ * @package    op-skeleton-2023
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	init
+$git_root    = trim(`git rev-parse --show-toplevel`);
+$local_hooks = 'asset/git/hooks/';
+
+//	Set local hooks
+chdir($git_root);
+if( $hooks_dir = trim(`git config --get core.hooksPath` ?? '') ){
+	echo "Already set custom local hooks: {$hooks_dir}\n";
+}else{
+	`git config --local core.hooksPath {$local_hooks}`;
+}
+
+//	Loop to each submodules.
+foreach( ['unit','layout','module','webpack'] as $base ){
+	//	...
+	chdir($git_root);
+	//	...
+	foreach( glob("asset/{$base}/*") as $path ){
+		//	...
+		chdir("{$git_root}/{$path}");
+		//	...
+		echo getcwd() . "\n";
+		//	...
+		`git config --local core.hooksPath {$git_root}/{$local_hooks}`;
+	}
+}
